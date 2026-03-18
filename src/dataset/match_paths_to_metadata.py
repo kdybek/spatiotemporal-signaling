01b68a3d5ab37c02d68bde3cdbb35209ecdf5e62
kdybek <kam.dyb1@gmail.com>
2026-03-18 11:27:08 +0100

    added more matching rules

--- src/dataset/match_paths_to_metadata.py
+++ src/dataset/match_paths_to_metadata.py
@@ -15,7 +15,7 @@ SOURCE_DIRS = [
 
 
 def find_metadata_position(exp_df, tiff_filename, tiff_full_path):
-    pattern = re.compile(r"^(\d+)(?:_Ori)?\.tiff?$")  # Matches "01.tif", "01_Ori.tif", etc.
+    pattern = re.compile(r"^(?:Series_?)?(\d+)(?:_Ori)?\.tiff?$")  # Matches "01.tif", "01_Ori.tif", etc.
 
     if match := pattern.match(tiff_filename):
         position = int(match.group(1))
