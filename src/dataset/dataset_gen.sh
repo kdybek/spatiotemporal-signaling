cd86e8a011c565f97718688394e55ece7b871a09
kdybek <kam.dyb1@gmail.com>
2026-03-25 20:43:34 +0100

    added slurm script

---
+++ src/dataset/dataset_gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=dataset_gen
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+#SBATCH --time=24:00:00
+#SBATCH --partition=cpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+
+cd /home/zppmimuw/spatiotemporal-signaling/src/dataset
+source .venv/bin/activate
+
+python dataset_gen.py --input=matched.pkl --output=/mnt/imaging.data/zppmimuw/spatiotemp_full_vid.zarr
