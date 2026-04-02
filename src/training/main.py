6bc8ef1675c414c151e828303c9ed224890022c6
kdybek <kam.dyb1@gmail.com>
2026-04-02 12:49:09 +0200

    updated training script

--- src/training/main.py
+++ src/training/main.py
@@ -9,7 +9,7 @@ import random
 import os
 import wandb
 
-from utils.datasets import N5VideoDataset
+from utils.datasets import ZarrVideoDataset
 from utils.logging import get_exp_name, setup_wandb
 
 
@@ -21,7 +21,25 @@ flags.DEFINE_integer('seed', 0, 'Random seed.')
 flags.DEFINE_integer('epochs', 5, 'Number of training epochs.')
 flags.DEFINE_integer('eval_interval', 1, 'Evaluation interval.')
 flags.DEFINE_integer('save_interval', 5, 'Saving interval.')
-flags.DEFINE_string('dataset_path', 'dataset.h5', 'Path to the dataset.')
+flags.DEFINE_string('train_dataset_path', 'train.zarr', 'Path to the train dataset.')
+flags.DEFINE_string('test_dataset_path', 'test.zarr', 'Path to the test dataset.')
+flags.DEFINE_string('save_dir', 'checkpoints', 'Directory to save model checkpoints.')
+
+
+def evaluate(model, dataloader, device, config):
+    model.eval()
+    total_loss = 0.0
+    with torch.no_grad():
+        for videos in tqdm(dataloader):
+            videos = videos.to(device)
+
+            mask = get_random_mask(videos.size(0), get_seq_len(config)).to(device)
+            outputs = model(pixel_values=videos, bool_masked_pos=mask)
+
+            total_loss += outputs.loss.item() * videos.size(0)
+
+    avg_loss = total_loss / len(dataloader.dataset)
+    return avg_loss
 
 
 def set_seed(seed):
@@ -62,25 +80,41 @@ def main(_):
                 group=FLAGS.run_group, name=exp_name)
     set_seed(FLAGS.seed)
 
-    dataset = N5VideoDataset(
-        h5_path=FLAGS.dataset_path,
-        num_frames=16,
+    train_dataset = ZarrVideoDataset(
+        zarr_path=FLAGS.train_dataset_path,
+        dataset_key="Data",
     )
 
-    dataloader = DataLoader(
-        dataset,
+    test_dataset = ZarrVideoDataset(
+        zarr_path=FLAGS.test_dataset_path,
+        dataset_key="Data",
+    )
+
+    train_dataloader = DataLoader(
+        train_dataset,
         batch_size=4,
         shuffle=True,
         num_workers=4,
         pin_memory=True
     )
 
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=4,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    T, C, H, W = train_dataset[0].shape
+    assert H == W, "Only square images are supported."
+
     config = VideoMAEConfig(
-        num_frames=16,
-        image_size=256,
-        num_channels=1,
+        num_frames=T,
+        image_size=H,
+        num_channels=C,
     )
     seq_len = get_seq_len(config)
 
@@ -91,13 +125,15 @@ def main(_):
 
     epochs = FLAGS.epochs
 
-    model.train()
+    eval_loss = evaluate(model, test_dataloader, device, config)
+    wandb.log({'eval_loss': eval_loss}, step=0)
 
-    for epoch in range(epochs):
-        for videos in tqdm(dataloader):
+    for epoch in range(1, epochs + 1):
+        model.train()
+        for videos in tqdm(train_dataloader):
+
             videos = videos.to(device)  # (B,T,C,H,W)
 
-            # HuggingFace expects (B,T,C,H,W)
             mask = get_random_mask(videos.size(0), seq_len).to(device)
             outputs = model(pixel_values=videos, bool_masked_pos=mask)
 
@@ -108,6 +144,15 @@ def main(_):
             optimizer.step()
 
             wandb.log({'train_loss': loss.item()})
+
+        if epoch % FLAGS.eval_interval == 0:
+            eval_loss = evaluate(model, test_dataloader, device, config)
+            wandb.log({'eval_loss': eval_loss}, step=epoch)
+
+        if epoch % FLAGS.save_interval == 0:
+            model_save_path = os.path.join(FLAGS.save_dir, f'model_epoch_{epoch}.pt')
+            torch.save(model.state_dict(), model_save_path)
+            wandb.save(model_save_path)
 
 
 if __name__ == '__main__':
--- src/training/utils/datasets.py
+++ src/training/utils/datasets.py
@@ -1,18 +1,27 @@
-import h5py
+import zarr
 import numpy as np
 from torch.utils.data import Dataset
 
 
-class N5VideoDataset(Dataset):
+def normalize_video(video):
+    video = video.astype("float32")
+
+    for c in range(video.shape[1]):  # per channel
+        p1, p99 = np.percentile(video[:, c], (1, 99))
+        video[:, c] = np.clip(video[:, c], p1, p99)
+        video[:, c] = (video[:, c] - p1) / (p99 - p1)
+
+    return video
+
+
+class ZarrVideoDataset(Dataset):
     def __init__(
         self,
-        h5_path,
-        dataset_key="videos",
-        num_frames=16,
+        zarr_path,
+        dataset_key="Data",
     ):
-        self.root = h5py.File(h5_path, "r")
+        self.root = zarr.open(zarr_path, mode="r")
         self.dataset_key = dataset_key
-        self.num_frames = num_frames
 
     def __len__(self):
         return self.root[self.dataset_key].shape[0]
@@ -20,18 +29,6 @@ class N5VideoDataset(Dataset):
     def __getitem__(self, idx):
         video = self.root[self.dataset_key][idx, ...]  # (T,C,H,W)
 
-        T = video.shape[0]
-
-        # Random temporal sampling
-        if T >= self.num_frames:
-            indices = np.linspace(0, T - 1, self.num_frames).astype(int)
-            video = video[indices]
-        else:
-            # pad if too short
-            pad = self.num_frames - T
-            pad_frames = np.repeat(video[-1:], pad, axis=0)
-            video = np.concatenate([video, pad_frames], axis=0)
-
-        video = video.astype(np.float32) / 255.0  # Normalize to [0,1]
+        video = normalize_video(video)
 
         return video
