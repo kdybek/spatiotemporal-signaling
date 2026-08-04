6c26401ca105b4bcb651c32a2bb8d7a584bdd2c4
kdybek <kam.dyb1@gmail.com>
2026-08-04 13:10:08 +0200

    updated rvm run script

--- src/training/RVM/scripts/run.sh
+++ src/training/RVM/scripts/run.sh
@@ -6,9 +6,9 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:rtx6000:1
 
-#SBATCH --time=96:00:00
+#SBATCH --time=256:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
 
 export SCRATCH=~/myscratch
@@ -17,14 +17,16 @@ export DATA_DIR=~/myimaging
 export XDG_CACHE_HOME=$SCRATCH/.cache
 export WANDB_API_KEY=$(cat ~/.wandb_key)
 
-cd ~/spatiotemporal-signaling/src/training/RVM
+cd $SCRATCH/spatiotemporal-signaling/src/training/RVM
+git pull
 source .venv/bin/activate
 
-python main.py --dataset_path $DATA_DIR/geminin_drugs_full_vid_2.zarr \
-               --run_group rvm \
-               --save_dir checkpoints/rvm \
-               --steps 100000 \
-               --eval_interval 1000 \
-               --save_interval 10000 \
-               --train_split 0.8 \
+python main.py --dataset_path $DATA_DIR/geminin_drugs_full_vid_3.zarr \
+               --run_group rvm_200K_2Ch \
+               --save_dir checkpoints/rvm_200K_2Ch \
+               --channel_names "Ch_ERK-KTR Ch_H2B" \
+               --steps 200000 \
+               --eval_interval 5000 \
+               --save_interval 20000 \
                --batch_size 16
+
