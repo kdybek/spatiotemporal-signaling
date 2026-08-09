604d960f314204aa85709eb1a32b829b56c4efa8
kdybek <kam.dyb1@gmail.com>
2026-08-09 15:43:51 +0200

    added cp_from_scratch script

---
+++ src/training/RVM/scripts/cp_from_scratch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=cp_from_scratch
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+#SBATCH --partition=all
+#SBATCH --nodelist=izbdodoma
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+
+export SCRATCH=~/myscratch
+
+export XDG_CACHE_HOME=$SCRATCH/.cache
+export WANDB_API_KEY=$(cat ~/.wandb_key)
+
+cp -r $SCRATCH/spatiotemporal-signaling/src/training/RVM/checkpoints/* ~/spatiotemporal-signaling/src/training/RVM/checkpoints/
